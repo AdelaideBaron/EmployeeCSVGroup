cc34e6e684540cd5bf41ce97581d6378b80650b6
Lee Halperin <106064455+leehalperin@users.noreply.github.com>
2022-06-13 16:11:12 +0100

    Add files via upload

---
+++ src/main/java/com/sparta/lh/EmployeeDTO.java
@@ -0,0 +1,57 @@
+package com.sparta.lh;
+
+
+import org.apache.wicket.markup.transformer.XsltOutputTransformerContainer;
+
+import java.util.Arrays;
+
+public class EmployeeDTO {
+    public static String[] employeeId;
+    //want to create fields that represent CSV file
+    //in CSV all values seen as strings
+    //below are fields that represent entries for a given employee
+    private String empId;
+    private String namePrefix;
+    private String firstName;
+    private String middleInitial;
+    private String lastName;
+    private String gender;
+    private String email;
+    private String dob; //private LocalDate dob;
+    private String dateOfJoining;
+    private String salary;
+    public EmployeeDTO(String empId, String namePrefix, String firstName, String middleInitial, String lastName,
+                       String gender, String email, String dob, String dateOfJoining, String salary) {
+        this.empId = empId;
+        this.namePrefix = namePrefix;
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.email = email;
+        this.dob = dob;
+        this.dateOfJoining = dateOfJoining;
+        this.salary = salary;
+    }
+    public EmployeeDTO (String[] fileRecord) {
+        this.empId = fileRecord[0];
+        this.namePrefix = fileRecord[1];
+        this.firstName = fileRecord[2];
+        this.middleInitial = fileRecord[3];
+        this.lastName = fileRecord[4];
+        this.gender = fileRecord[5];
+        this.email = fileRecord[6];
+        this.dob = fileRecord[7];
+        this.dateOfJoining = fileRecord[8];
+        this.salary = fileRecord[9];
+    }
+
+
+    public String[] getEmpId() {
+        String[] employeeId = new String[] {(empId)};
+        return employeeId;
+    }
+
+
+
+}
